3dd7e7bba4cf51d91bb220891852678b77ef267c
Aaron Schlesinger <arschles@gmail.com>
2017-12-10 17:02:38 -0800

    putting scripts inline

--- .gcs_activate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ash
-
-echo $GOOGLE_JSON >> secret.json
-CLOUDSDK_PYTHON_SITEPACKAGES=1 gcloud auth activate-service-account $GOOGLE_ACCOUNT \
-    --key-file secret.json
--- .gcs_upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-
-# upload cross-compiled link checker binaries
-gsutil -m cp cross/* gs://$GOOGLE_BUCKET/*
-# set uploaded binaries to readable by the public
-gsutil -m acl set public-read gs://$GOOGLE_BUCKET/*
-# force all objects to become immediately consistent
-gsutil -m setmeta -h "Cache-Control:public, max-age=0" gs://$GOOGLE_BUCKET/*
