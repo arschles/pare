471e0688444631520daf692e9bc4326e415fc85d
Aaron Schlesinger <arschles@gmail.com>
2017-12-10 16:17:36 -0800

    adding cross-compilation

--- scripts/upload-to-gcs.sh
+++ scripts/upload-to-gcs.sh
@@ -1,6 +1,6 @@
 # upload cross-compiled link checker binaries
-gsutil -m cp pare gs://$GOOGLE_BUCKET/pare
+gsutil -m cp cross/* gs://$GOOGLE_BUCKET/*
 # set uploaded binaries to readable by the public
-gsutil -m acl set public-read gs://$GOOGLE_BUCKET/pare
+gsutil -m acl set public-read gs://$GOOGLE_BUCKET/*
 # force all objects to become immediately consistent
-gsutil -m setmeta -h "Cache-Control:public, max-age=0" gs://$GOOGLE_BUCKET/pare
+gsutil -m setmeta -h "Cache-Control:public, max-age=0" gs://$GOOGLE_BUCKET/*
